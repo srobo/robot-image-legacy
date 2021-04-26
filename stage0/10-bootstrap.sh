3f77787218f37d7240d81cbf13a94844852bf2c2
Karina Kwiatek <mh@moltenheim.net>
2021-04-26 22:38:07 +0200

    Add git tag to build metadata

--- stage0/10-bootstrap.sh
+++ stage0/10-bootstrap.sh
@@ -26,6 +26,7 @@ info "Creating build metadata"
   echo BUILD_HOST="$(hostname)"
   echo BUILD_TIME="$(date)"
   echo BUILD_COMMIT="$(git rev-parse --short HEAD)"
+  echo BUILD_TAG="$(git describe --exact-match --tags)"
   if [ -n "$GITHUB_ACTIONS" ]; then
     echo GITHUB_ACTOR="${GITHUB_ACTOR}"
     echo GITHUB_REF="${GITHUB_REF}"
