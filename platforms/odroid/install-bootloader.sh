c81d5b4e824a57e4db3ce6c7454e6197df34ba88
Karina Kwiatek <mh@moltenheim.net>
2021-05-08 14:28:07 +0200

    stage0: Use pushed dir to get script root

--- platforms/odroid/install-bootloader.sh
+++ platforms/odroid/install-bootloader.sh
@@ -2,6 +2,6 @@
 set -e
 
 pushd "$BUILD_DIR/boot"
-mkimage -A arm -O linux -T script -C none -n "U-Boot boot script" -d "${oldpwd}/platforms/odroid/boot.txt" "$BUILD_DIR/boot/boot.scr"
+mkimage -A arm -O linux -T script -C none -n "U-Boot boot script" -d "$(dirs +0)/platforms/odroid/boot.txt" "$BUILD_DIR/boot/boot.scr"
 bash "$BUILD_DIR/boot/sd_fusing.sh" "$OUTPUT_DEVICE"
 popd
