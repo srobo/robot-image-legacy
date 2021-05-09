938d57d3d06d3006035cb6604863377218bb2c5b
Karina Kwiatek <mh@moltenheim.net>
2021-05-10 01:00:33 +0200

    ci: Install pacstrap & pacman

--- stage0/01-host_pacstrap.sh
+++ stage0/01-host_pacstrap.sh
@@ -4,6 +4,11 @@ set -e
 source ./util.sh
 
 export CARCH=armv7l
+
+info "Setting up keyring"
+mkdir -p "$BUILD_DIR/etc/pacman.d/gnupg"
+LIBRARY="$PWD/tools/usr/share/makepkg" pacman-key --init --config "$PWD/tools/etc/pacman.conf" --gpgdir "$PWD/tools/etc/pacman.d/gnupg"
+
 info "Bootstrapping" "Arch Linux ARM"
 # Reads in common and platform packages and runs pacstrap with them
 pacstrap -MGC stage0/pacman.conf "$BUILD_DIR" base
