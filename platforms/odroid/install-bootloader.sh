8d7bddba7e5adeb4bf402856a3d8c1f6bac1bf65
Karina Kwiatek <mh@moltenheim.net>
2021-05-11 20:22:56 +0200

    odroid: Fix lint error

--- platforms/odroid/install-bootloader.sh
+++ platforms/odroid/install-bootloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cp platforms/$PLATFORM/boot.txt "$BUILD_DIR/boot/boot.txt"
+cp "platforms/$PLATFORM/boot.txt" "$BUILD_DIR/boot/boot.txt"
 pushd "$BUILD_DIR/boot"
 mkimage -A arm -O linux -T script -C none -n "U-Boot boot script" -d boot.txt boot.scr
 bash "$BUILD_DIR/boot/sd_fusing.sh" "$OUTPUT_DEVICE"
