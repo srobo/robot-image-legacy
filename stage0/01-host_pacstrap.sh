73f77e4b6f9da85f2226bd100bcf9f3f4e910914
Karina Kwiatek <mh@moltenheim.net>
2021-05-15 00:23:33 +0200

    stage0: Remove snapper

--- stage0/01-host_pacstrap.sh
+++ stage0/01-host_pacstrap.sh
@@ -5,8 +5,8 @@ source ./util.sh
 
 export CARCH=armv7l
 info "Bootstrapping" "Arch Linux ARM"
-# bootstraps the system, installing the base and snapper packages
-pacstrap -cMGC stage0/pacman.conf "$BUILD_DIR" base snapper
+# bootstraps the system, installing the base package
+pacstrap -cMGC stage0/pacman.conf "$BUILD_DIR" base
 
 info "Generating" "/etc/fstab"
 genfstab -U "$BUILD_DIR" > "$BUILD_DIR/etc/fstab"
